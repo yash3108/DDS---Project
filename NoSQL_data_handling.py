9850731f231f79297b7e4df673b5eca93fa42b50
Niraj Sonje <nirajs0607@gmail.com>
2023-11-20 15:45:51 -0700

    Placing orders in local DB

---
+++ NoSQL_data_handling.py
@@ -0,0 +1,79 @@
+import pymongo
+from pymongo import MongoClient
+
+def getCollection(db, collection):
+    """Load feature descriptor collection from MongoDB"""
+    client = MongoClient("mongodb://localhost:27017")
+    return client[db][collection]
+
+def show_order(order_id):
+    order_collection = getCollection("The_Devils_Base", "order_collection")
+    order = order_collection.find_one({"order_id": order_id})
+    print(order)
+
+def get_order(order_collection, order_id):
+    # order_collection = getCollection("The_Devils_Base", "order_collection")
+    order = order_collection.find_one({"order_id": order_id})
+    return order
+
+def show_product_components(product_id):
+    product_collection = getCollection("The_Devils_Base", "product_collection")
+    product = product_collection.find_one({"product_id": product_id})
+    print(product)
+
+def get_product_components(product_collection, product_id):
+    product = product_collection.find_one({"product_id": product_id})
+    return product["components"]
+
+def insert_order():
+    order_collection = getCollection("The_Devils_Base", "order_collection")
+    def get_next_order_id():
+        max_order = order_collection.find_one(sort=[("order_id", pymongo.DESCENDING)])
+        if max_order:
+            return max_order["order_id"] + 1
+        else:
+            return 1
+        
+    products = []
+    while True:
+        product_id = input("Enter product ID (or type 'done' to finish): ")
+        if product_id.lower() == 'done':
+            break
+        quantity = int(input("Enter quantity for given Product ID ({}): ".format(product_id)))
+        products.append({"product_id": product_id, "quantity": quantity})
+    
+    order_data = {
+    "order_id": get_next_order_id(),
+    "products": products,
+    }
+    # Insert data into the orders collection
+    order_collection.insert_one(order_data)
+    return products
+
+def insert_product():
+    product_collection = getCollection("The_Devils_Base", "product_collection")
+    def get_next_product_id():
+        max_order = product_collection.find_one(sort=[("product_id", pymongo.DESCENDING)])
+        if max_order:
+            return max_order["product_id"] + 1
+        else:
+            return 1
+        
+    components = []
+    while True:
+        component_id = input("Enter component ID (or type 'done' to finish): ")
+        if component_id.lower() == 'done':
+            break
+        quantity = int(input("Enter quantity for given Component ID ({}): ".format(component_id)))
+        components.append({"component_id": component_id, "quantity": quantity})
+    
+    product_data = {
+    "product_id": get_next_product_id(),
+    "components": components,
+    }
+    # Insert data into the products collection
+    product_collection.insert_one(product_data)
+
+
+# insert_product()
+show_order(1)
---
+++ order_place.py
@@ -0,0 +1,73 @@
+import psycopg2
+from NoSQL_data_handling import *
+def display_products(conn): 
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+
+    # Define the SQL query to select product name and price from the product table
+    query = "SELECT product_name, price FROM product"
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all rows from the result set
+    rows = cursor.fetchall()
+
+    # Display the product information
+    for row in rows:
+        product_name, price = row
+        print(f"Product N: {product_name}, Price: {price}")
+
+def get_connection(region):
+    return conn
+
+def place_order(conn):
+    display_products(conn)
+    products = insert_order()
+    rem_products = {}
+    for product_id in products:
+        query = f"SELECT quantity from product where product_id = '{product_id}'",
+        cursor = conn.cursor()
+        cursor.execute(query)
+        available_qty = cursor.fetchall()
+        if products[product_id] <= available_qty:
+            continue
+        else:
+            rem_products[product_id] = products[product_id] - available_qty
+        
+    if not len(rem_products):
+        print("Order Placed: All products available for immediate delivery")
+        return
+    
+    for product_id in rem_products:
+        rem_components = {}
+        client = getCollection(region_db, "product_collection")
+        components = get_product_components(client, product_id)
+        for component_id in components:
+            query = f"SELECT quantity from inventory where component_id = '{component_id}'"
+            cursor.execute(query)
+            available_qty = cursor.fetchall()
+            if components[component_id] <= available_qty:
+                continue
+            else:
+                rem_components[component_id] = components[component_id] - available_qty
+        if not len(rem_components):
+            print("Order Placed: All Products are not available manufacturing remaining products from components available in warehouse")
+            return
+    collection = getCollection(region_db, "supplier_collection")
+    for component_id in rem_components:
+        query = f"SELECT component_name from Inventory where component_id = {component_id}"
+        cursor.execute(query)
+        component_name = cursor.fetchall()
+        print(f"Missing component: {component_name} Quantity: {rem_components[component_id]}" )
+        print("Suppliers for the component")
+        result = collection.find({'components.component_id': component_id})
+        for supplier in result:
+            print(supplier)
+        
+
+                    
+
+
+    
+        
