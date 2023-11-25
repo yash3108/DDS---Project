01722fa226d4b4341b6e99f7c85f2be80d9bdb31
Yash Shah <shahyash3108@gmail.com>
2023-11-25 15:44:41 -0700

    ACID properties bug fix

--- NoSQL_data_handling.py
+++ NoSQL_data_handling.py
@@ -83,9 +83,10 @@ def insert_new_components(component_list):
         if component_id.lower() == 'done':
             break
         if int(component_id) not in component_list:
-            print("This Component doesn't exists in the Inventory: ")
-        quantity = int(input("Enter quantity for given Component ID ({}): ".format(component_id)))
-        components.append({"component_id": component_id, "quantity": quantity})
+            print("This Component doesn't exists in the Inventory")
+        else:
+            quantity = int(input("Enter quantity for given Component ID ({}): ".format(component_id)))
+            components.append({"component_id": component_id, "quantity": quantity})
 
     return components
 
