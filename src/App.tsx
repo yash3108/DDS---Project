e16def3d8caef805da052d8eadc03ea35419f9bf
Niraj Sonje <nirajs0607@gmail.com>
2023-12-10 22:44:01 -0700

    Sign In with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
 import './App.css';
 
-function App() {
+Modal.setAppElement('#root');
+
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCallbackResponse = (response: any) => {
+    console.log("Encoded JWT ID token: " + response.credential);
+    closeModal();
+  };
+
+  useEffect(() => {
+    (window as any).google.accounts.id.initialize({
+      client_id: "695051548996-74f2sq1fil098b34lddqrmq2t8qlflr2.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+  }, []);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    if (isModalOpen) {
+      timeoutId = setTimeout(() => {
+        (window as any).google.accounts.id.renderButton(
+          document.getElementById("signInDiv"),
+          { theme: "outline", size: "large" }
+        );
+      }, 200);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isModalOpen]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={openModal}>Open Sign In</button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        shouldCloseOnOverlayClick={true}
+        contentLabel="Sign In Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div>
+          <h2>Sign In with Google</h2>
+          <div id="signInDiv"></div>
+        </div>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
